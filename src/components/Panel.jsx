3a0858c8ee1677c8ad3035e5e307ead6090cdbc2
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2024-01-08 09:23:26 -0500

    Move SubPanel component to separate file

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
 import { camelToTitleCase } from '../util.js';
+import SubPanel from './SubPanel.jsx';
 import Form from './Form.jsx';
 
 import '../styles/Panel.css';
@@ -20,67 +20,6 @@ export default function Panel({ title, isActive, onClick, data, setData }) {
   );
 }
 
-function SubPanel({ type, data, setData }) {
-  const [formVisible, setFormVisible] = useState(false);
-  const [formId, setFormId] = useState('');
-
-  function handleAdd() {
-    const newData = { ...data };
-    const newId = crypto.randomUUID();
-    newData[type].push({ id: newId });
-
-    setData(newData);
-    setFormId(newId);
-    setFormVisible(true);
-  }
-
-  const handleSave = () => setFormVisible(false);
-  function handleDelete() {
-    if (window.confirm('Are you sure you want to delete this?')) {
-      // get index of subData w/ id in data
-      const index = data[type].map(subData => subData.id).indexOf(formId);
-
-      // splice subData from data
-      const newData = { ...data };
-      newData[type].splice(index, 1);
-      setData(newData);
-
-      setFormVisible(false);
-    }
-  }
-
-  function handleSubPanelButtonClick(e) {
-    setFormId(e.target.id);
-    setFormVisible(true);
-  }
-
-  if (formVisible)
-    return (
-      <div className="sub-panel">
-        <Form type={type} data={data} setData={setData} id={formId} />
-        <div className="buttons">
-          <button onClick={handleDelete}>Delete</button>
-          <button onClick={handleSave}>Save</button>
-        </div>
-      </div>
-    );
-
-  return (
-    <div className="sub-panel">
-      {/* show existing education/experience */}
-      {data[type].map((entry) => (
-        <SubPanelButton
-          key={entry.id}
-          entry={entry}
-          onClick={handleSubPanelButtonClick}
-        />
-      ))}
-      {/* on click, show relevant form */}
-      <button onClick={handleAdd}>Add {type}</button>
-    </div>
-  );
-}
-
 function PanelButton({ title, isActive, onClick }) {
   return (
     <button className="expand-panel" onClick={onClick}>
@@ -93,14 +32,6 @@ function PanelButton({ title, isActive, onClick }) {
         <title>chevron-down</title>
         <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
       </svg>
-    </button>
-  );
-}
-
-function SubPanelButton({ entry, onClick }) {
-  return (
-    <button id={entry.id} className="expand-sub-panel" onClick={onClick}>
-      {entry.school || entry.company}
     </button>
   );
 }
---
+++ src/components/SubPanel.jsx
@@ -0,0 +1,73 @@
+import { useState } from 'react';
+import Form from './Form.jsx';
+
+import '../styles/SubPanel.css';
+
+export default function SubPanel({ type, data, setData }) {
+  const [formVisible, setFormVisible] = useState(false);
+  const [formId, setFormId] = useState('');
+
+  function handleAdd() {
+    const newData = { ...data };
+    const newId = crypto.randomUUID();
+    newData[type].push({ id: newId });
+
+    setData(newData);
+    setFormId(newId);
+    setFormVisible(true);
+  }
+
+  const handleSave = () => setFormVisible(false);
+  function handleDelete() {
+    if (window.confirm('Are you sure you want to delete this?')) {
+      // get index of subData w/ id in data
+      const index = data[type].map((subData) => subData.id).indexOf(formId);
+
+      // splice subData from data
+      const newData = { ...data };
+      newData[type].splice(index, 1);
+      setData(newData);
+
+      setFormVisible(false);
+    }
+  }
+
+  function handleSubPanelButtonClick(e) {
+    setFormId(e.target.id);
+    setFormVisible(true);
+  }
+
+  if (formVisible)
+    return (
+      <div className="sub-panel">
+        <Form type={type} data={data} setData={setData} id={formId} />
+        <div className="buttons">
+          <button onClick={handleDelete}>Delete</button>
+          <button onClick={handleSave}>Save</button>
+        </div>
+      </div>
+    );
+
+  return (
+    <div className="sub-panel">
+      {/* show existing education/experience */}
+      {data[type].map((entry) => (
+        <SubPanelButton
+          key={entry.id}
+          entry={entry}
+          onClick={handleSubPanelButtonClick}
+        />
+      ))}
+      {/* on click, show relevant form */}
+      <button onClick={handleAdd}>Add {type}</button>
+    </div>
+  );
+}
+
+function SubPanelButton({ entry, onClick }) {
+  return (
+    <button id={entry.id} className="expand-sub-panel" onClick={onClick}>
+      {entry.school || entry.company}
+    </button>
+  );
+}
